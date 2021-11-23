942d204dd22fc04f33464fd1e7c9674872156dad
Evgen-Mizin <git.mizin@mail.ru>
2021-11-23 21:48:33 +0300

    task1

---
+++ js-basics/task1.js
@@ -0,0 +1,12 @@
+let value = Math.floor(Math.PI * 100) / 100 ;
+
+let valueMin = Math.min(15,11,16,12,51,12,13,51);
+let valueMax = Math.max(15,11,16,12,51,12,13,51);
+
+let valueRandom = Math.floor(Math.random() * 100) / 100 ;
+
+let value = Math.floor(Math.random() * 10 + 1);
+
+let value = (0.6 * 10 + 0.7 * 10) / 10;
+
+let value = parseInt('100$');
