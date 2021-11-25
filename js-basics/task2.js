41e55ccdfe7bd18d4b16691ec5dd7b5f4fede01c
Evgen-Mizin <git.mizin@mail.ru>
2021-11-25 20:13:42 +0300

    task2

---
+++ js-basics/task2.js
@@ -0,0 +1,19 @@
+let value1 = string[0];
+let value2 = string[string.length - 1];
+
+let value1 = string[0].toUpperCase();
+let value2 = string[string.length - 1].toUpperCase();
+
+let value = string.indexOf('string');
+
+let value1 = string.indexOf(' ');
+let value2 = string.indexOf(' ', firstSpace + 1);
+
+let value = string.substr(5, 4);
+
+let value = string.slice(5, 10);
+
+let value = string.slice(0, -6);
+
+let a = 20, b = 16;
+let value = String(a) + String(b);
